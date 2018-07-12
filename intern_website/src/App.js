67dad849e19ff47bb4829a3e3babae7459420b0f
Michael Wulber <mwulber2015@ufl.edu>
2018-07-12 15:47:37 -0400

    created Header, Footer, Main components

--- intern_website/src/App.js
+++ intern_website/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>intern website!</h1>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
