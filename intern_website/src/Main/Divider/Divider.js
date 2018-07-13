43fa0ca41abbab39c9389567760acbbdfbef120d
morgansturtz <msturtz@my.apsu.edu>
2018-07-13 14:14:36 -0400

    Pointer on links, width correction

--- intern_website/src/Main/Divider/Divider.js
+++ intern_website/src/Main/Divider/Divider.js
@@ -3,7 +3,7 @@ import './Divider.css';
 
 const Divider = (props) =>
   <div className="divider">
-    <img src={props.filename} alt="divider"></img>
+    <img src={props.filename} alt="divider" width="100%"></img>
   </div>
 
 export default Divider;
