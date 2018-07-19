1af4f9d0818c85775f5deab6d867ba2df6705150
Michael Wulber <mwulber2015@ufl.edu>
2018-07-19 13:46:21 -0400

    merge issues

--- intern_website/src/Config/PathConfig.js
+++ intern_website/src/Config/PathConfig.js
@@ -2,27 +2,27 @@ const IT_PATHS = [
   {
     title: 'Infrastructure',
     description: 'Interns for this program will be focused on monitoring, installation, configuration, debugging, administration, troubleshooting and maintenance of technology solutions.',
-    css_class: 'path_1',
+    cssClass: 'path_1',
   },
   {
     title: 'Innovation',
     description: 'Interns for this program will be focused on creating new innovative designs and front-end prototypes for Florida Blue.',
-    css_class: 'path_2',
+    cssClass: 'path_2',
   },
   {
     title: 'Data Analytics',
     description: 'Interns for this program will be focused on data analytics, business Intelligence and data quality assessment for Florida Blue.',
-    css_class: 'path_3',
+    cssClass: 'path_3',
   },
   {
     title: 'Security',
     description: 'Interns for this program will be focused on activities that ensure all users in the organization have the appropriate levels of access to applications, systems and data resources.',
-    css_class: 'path_4',
+    cssClass: 'path_4',
   },
   {
     title: 'Application Development',
     description: 'Interns for this program will be focused on development of new applications with basic programming knowledge of JavaScript, HTML, CSS, or other OOP experience, excellent written and verbal communication skills used in development of application and requirements gathering process.',
-    css_class: 'path_5',
+    cssClass: 'path_5',
   },
 ]
 
